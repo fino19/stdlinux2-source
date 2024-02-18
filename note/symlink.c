f41c9ae65009beb7d79e9ba997f865fd03bdbc04
fino19 <75620341+fino19@users.noreply.github.com>
2024-02-18 10:07:09 +0900

    2024-02-16

--- note/symlink.c
+++ note/symlink.c
@@ -5,12 +5,12 @@
 int main(int argc, char *argv[])
 {
     if (argc != 3) {
-        fprintf(stderr, "%s: wrong arguments\n", argv[0]);
+        fprintf(stderr, "%s: wrong number of arguments\n", argv[0]);
         exit(1);
-    }   
-    if (link(argv[1], argv[2]) < 0) {
+    }
+    if (symlink(argv[1], argv[2]) < 0) {
         perror(argv[1]);
         exit(1);
     }
-    exit(1);
+    exit(0);
 }
